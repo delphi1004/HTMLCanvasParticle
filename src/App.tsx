538548d183d8989c55866db77a6d85a804d588c9
delphi1004 <delphi1004@hanmail.net>
2022-01-01 13:51:18 +0900

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useRef, useState } from 'react';
+import { KeyboardEvent, useEffect, useRef, useState } from 'react';
 import './App.css';
 import {Particle} from './Particle'
+
+let text = ''
+let mouseDown = false
 
 function App() {
   let particle:Particle[] = []
@@ -9,19 +12,93 @@ function App() {
   let ctx:CanvasRenderingContext2D
 
   const ref = useRef<HTMLCanvasElement>(null)
+  const helloref = useRef<HTMLCanvasElement>(null)
   const [context,setContext] = useState<CanvasRenderingContext2D>()
+
+ // const [text,setText] = useState('A')
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const animate = () =>{
     if(ctx){
-      ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height)
-      ctx.fillStyle = 'red'
-      ctx.strokeStyle = "#000000";
-      ctx.lineWidth = 0.1;
-      for(let i=0;i<particle.length;i++)
-      particle[i].draw()
+      let lineCount = 0
+      //ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height)
+      ctx.fillStyle = 'rgba(0, 0, 0, 0.4)'
+      ctx.fillRect(0,0,1000,1000)
+      ctx.fillStyle = 'white'
+      ctx.strokeStyle = '#000000';
+      ctx.lineWidth = 0.5;
+      ctx.font = '30px Arial'
+      ctx.fillText(text, 10, 50)
+   
+      for(let i=0;i<particle.length;i++){
+        particle[i].draw(mouseDown)
+        if(!mouseDown){
+          for(let j=i;j<particle.length;j++){
+            let dx = particle[i].pos.x - particle[j].pos.x
+            let dy = particle[i].pos.y - particle[j].pos.y
+            let d = Math.sqrt(dx*dx+dy*dy)
+
+            if(d < 50){
+              lineCount++
+              ctx.strokeStyle='rgba(255,255,255,0.2)'
+              ctx.beginPath();
+              ctx.moveTo(particle[i].pos.x, particle[i].pos.y);
+              ctx.lineTo( particle[j].pos.x,  particle[j].pos.y);
+              ctx.stroke();
+              if(lineCount > 200){
+                break
+              }
+            }
+          }
+        }
+      }
     }
     requestAnimationFrame(animate)
+  }
+
+  const createOffScreen = () =>{
+    const offscreenCanvas = document.createElement('canvas')
+    const context = offscreenCanvas.getContext('2d')
+    const dpr = window.devicePixelRatio || 1
+    const wWidth = window.innerWidth
+    const wHeight = 55
+    offscreenCanvas!.width = wWidth * dpr
+    offscreenCanvas!.height = wHeight * dpr
+    offscreenCanvas!.style.width = wWidth+'px'
+    offscreenCanvas!.style.height = wHeight+'px'
+    context!.scale(dpr,dpr)
+    
+    //context!.setTransform(dpr,0,0,dpr,0,0)
+    //context!.scale(dpr,dpr)
+    context!.fillStyle = 'black'
+    context!.fillRect(0,0,offscreenCanvas.width,offscreenCanvas.height)
+    context!.fillStyle = 'white'
+    context!.font = "20px";
+    context!.fillText('DPC 2022' , 2,10)
+ 
+    const pixel = context!.getImageData(0,0, offscreenCanvas.width, offscreenCanvas.height).data
+  
+    let index = 0
+    let position = []
+    for(let i=0;i<pixel.length;i+=4){
+      if(pixel[i] > 50){
+        const x = index % offscreenCanvas!.width
+        const y = index / offscreenCanvas!.width
+        context!.fillStyle = 'red'
+        context!.fillRect(x,y+20,1,1)
+        position.push({x,y})
+      }
+      index++
+    }
+
+    return {offscreenCanvas,position}
+  }
+
+
+  const keyPressHandler =(e: string) =>{
+   // setText(e)
+    text += e
+    console.log(e)
   }
 
   useEffect(() => {
@@ -36,19 +113,27 @@ function App() {
         canvas!.style.width = wWidth+'px'
         canvas!.style.height = wHeight+'px'
         ctx!.scale(dpr,dpr)
-
         setContext(ctx!)
+        //createOffScreen()
+        const {offscreenCanvas,position} = createOffScreen()
+        //ctx.drawImage(offscreenCanvas , 0,0)
+
+        console.log('dpr',position)
 
         if(particle.length === 0){
-          for(let i=0;i<1000;i++){
-            particle.push(new Particle(ctx!))
+          for(let i=0;i<position.length;i++){
+            particle.push(new Particle(ctx!, position[i].x*3,position[i].y*3+wHeight/4))
           }
-          animate()
+         animate()
         }
     }
 
-    window.addEventListener('resize', windowSizer)
+    windowSizer()
 
+    window.addEventListener('resize', windowSizer)
+    window.addEventListener('mousedown',() => {mouseDown = true})
+    window.addEventListener('mouseup',() => {mouseDown = false})
+ 
     return () => {
         window.removeEventListener('resize', windowSizer) 
     }
@@ -56,10 +141,12 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
+    <div className='App'>
      <canvas id='canvas' ref={ref}></canvas>
+     <h1 id='hello'>hello</h1>
+     <input type='text' id='text' value={text} onChange={(e) => keyPressHandler(e.target.value)}/>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/Particle.tsx
+++ src/Particle.tsx
@@ -1,43 +1,139 @@
+
+interface Vector2D {
+    x:number
+    y:number
+} 
+
+//const color = ['hsl(214,53%,35%)','hsl(208,40%,55%)','hsl(11,59%,95%)','hsl(11,34%,65%)']
+//const color = ['hsl(207,94.8%,45.1%)','hsl(191,39.9%,76.9%)','hsl(203,50.2%,100%)','hsl(196,86.8%,84.7%)']
+const color = ['hsl(208,20%,65%)','hsl(39,19%,95%)','hsl(33,64%,95%)','hsl(22,80%,85%)','hsl(22,69%,55%)']
+
 export class Particle{
     ctx: CanvasRenderingContext2D
-    posX:number
-    posY:number
+    pos:Vector2D
+    orgPos:Vector2D
     dirX:number
     dirY:number
     speedX:number
     speedY:number
     size:number
+    curSize:number
+    velocity:Vector2D
+    acceleration:Vector2D
+    hue:number
+    dirSelected:boolean
+    colorIndex:number
 
-    constructor(ctx:CanvasRenderingContext2D){
+    constructor(ctx:CanvasRenderingContext2D,x:number,y:number){
         this.ctx = ctx
-        this.posX = Math.random() * ctx.canvas.width
-        this.posY = Math.random() * ctx.canvas.height
+        this.pos = {x:Math.random() * ctx.canvas.width,y:Math.random() * ctx.canvas.height}
+        this.orgPos = {x:x,y:y}
+   
+        this.velocity = {x:Math.random(),y:Math.random()}
+        this.acceleration = {x:0,y:0}
 
         this.speedX = Math.random()
         this.speedY = Math.random()
+        this.hue = (Math.random()*(40-200))+150
 
-        this.size = this.rand(1,5)
-        this.dirX = this.rand(0.5,2)
-        this.dirY = this.rand(0.7,2.1)
+        this.colorIndex = this.rand(0,color.length)
+
+        this.size = this.rand(1.1,1.2)
+        this.curSize = 0.1
+        this.dirX = this.rand(-2,2)
+        this.dirY = this.rand(-2,2)
+        this.dirSelected = true
     }
 
     rand(min:number, max:number) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    draw(){
+    attract(){
+      let force:Vector2D = {x:this.orgPos.x - this.pos.x,y:this.orgPos.y - this.pos.y}
+      //let force:Vector2D = {x:this.pos.x-this.orgPos.x , y: this.pos.y-this.orgPos.y}
+      let d = Math.sqrt(Math.pow(force.x,2) + Math.pow(force.y,2))
+
+      force.x /=d
+      force.y /=d
+  
+
+    //   force.x *=15
+    //   force.y *=15
+
+    //   const strength = (1 * 100 * 1) / (d * d)
+
+    //   force.x *= strength
+    //   force.y *= strength
+
+    //   force.x *= 0.1
+    //   force.y *= 0.1
+
+      let steer:Vector2D = {x:force.x - this.velocity.x , y:force.y - this.velocity.y}
+      let distanceSteer = Math.sqrt(Math.pow(steer.x,2) + Math.pow(steer.y,2))
+      steer.x = Math.min(Math.max(distanceSteer, 0.01), 0.1)
+      steer.y = Math.min(Math.max(distanceSteer, 0.01), 0.1)
+
+      this.acceleration.x += steer.x
+      this.acceleration.y += steer.y
+
+      this.velocity.x += this.acceleration.x
+      this.velocity.y += this.acceleration.y
+    //   this.velocity.x = Math.min(Math.max(this.velocity.x, 0.1), 0.2)
+    //   this.velocity.y = Math.min(Math.max(this.velocity.y, 0.1), 0.2)
+
+    //   this.pos.x += this.velocity.x 
+    //   this.pos.y += this.velocity.y
+
+    //   this.pos.x += force.x 
+    //   this.pos.y += force.y
+
+      this.acceleration.x *= 0
+      this.acceleration.y *= 0
+    }
+
+    draw(mouseDown:boolean){
+        this.attract()
+        
         this.ctx.beginPath();
-        this.ctx.arc(this.posX, this.posY, this.size, 0, 2 * Math.PI);
+        this.ctx.fillStyle = color[this.colorIndex]
+        this.ctx.arc(this.pos.x, this.pos.y, Math.abs(this.size), 0, 2 * Math.PI);
         this.ctx.fill();
 
-        this.posX +=this.dirX
-        this.posY +=this.dirY
+        // console.log('mouseDown',mouseDown)
 
-        if(this.posX >= this.ctx.canvas.width/3.0625 || this.posX <=0){
+        if(mouseDown){
+           this.dirX = (this.orgPos.x - this.pos.x) * 0.06
+           this.dirY = (this.orgPos.y - this.pos.y) * 0.05
+           this.dirSelected = true
+        }else if( this.dirSelected){
+            this.dirX = Math.random() * (Math.random() > 0.5 ? 1:-1)
+            this.dirY = Math.random() * (Math.random() > 0.5 ? 1:-1)
+            this.dirSelected = false
+            }
+
+        this.pos.x += this.dirX
+        this.pos.y += this.dirY
+
+        //this.size += this.curSize
+
+        if(this.dirX <=0){
+            this.pos.x += Math.random()*0.2
+        }
+
+        if(this.dirY <=0){
+            this.pos.y += Math.random()*0.2
+        }
+
+        if(this.size > 10 || this.size <= 0){
+            this.curSize *= -1
+        }
+
+        if(this.pos.x >= this.ctx.canvas.width/3.0625 || this.pos.x <=0){
             this.dirX *= -1
         }
 
-        if(this.posY >= this.ctx.canvas.height/3.0625 || this.posY <=0){
+        if(this.pos.y >= this.ctx.canvas.height/3.0625 || this.pos.y <=0){
             this.dirY *= -1
         }
     }
