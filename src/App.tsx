dfe034a7c7bc998dd559e04313cd8f22e4def8f3
delphi1004 <delphi1004@hanmail.net>
2023-05-12 12:22:33 +0300

    nitor

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {  useEffect, useRef } from 'react';
 import './App.css';
 import {Particle} from './Particle'
 
-let text = 'S Group'
+let text = 'Nitor'
 let mouseDown = false
 let particleCreated = false
 let mouseX = 0
